3eca84d99d9c2920f9ecfd8f653a72e61b7370f3
johnkmur <johnkmur@umich.edu>
2017-02-18 23:52:46 -0500

    added some testing

---
+++ tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+python main.py < test1.txt > out.txt
+
+echo "Test 1"
+
+python main.py < test2.txt > out.txt
+
+echo "Test 2"
+
+python main.py < test3.txt > out.txt
+
+echo "Test 3"
+
+python main.py < test4.txt > out.txt
+
+echo "Test 4"
